7d5362ee915449f966c6106d4816dc63639d28b6
MmdRza.ebrahimi <moh.ebrahimi@bluteam.ir>
2025-11-11 08:40:20 +0330

    feat: Initial project setup for Vending Machine with Docker Compose

---
+++ src/main/java/com/mrebrahimi/smart_vending_machine/SmartVendingMachineApplication.java
@@ -0,0 +1,13 @@
+package com.mrebrahimi.smart_vending_machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartVendingMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartVendingMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrebrahimi/smart_vending_machine/SmartVendingMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrebrahimi.smart_vending_machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartVendingMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
